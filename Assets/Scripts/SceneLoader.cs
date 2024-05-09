97efd6e16bc57b9f3312bdf3965a5da933f81fb8
Aleksandr Burykh <aleksandrburykh1989@gmail.com>
2024-05-09 12:00:40 +0300

    LevelBase prefab

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,8 +5,7 @@ using UnityEngine.SceneManagement;
 
 public abstract class SceneLoader : MonoBehaviour
 {
-	[SerializeField]
-	protected int _sceneVariants = 5;
+	protected readonly int _sceneVariants = 5;
 
 	public virtual void LoadLevel(int level)
 	{
