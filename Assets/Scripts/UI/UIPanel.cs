5083b586c9e64c59b938d79b02525dbcc0d7c255
Aleksandr Burykh <aleksandrburykh1989@gmail.com>
2024-05-10 18:17:46 +0300

    Additional animations

--- Assets/Scripts/UI/UIPanel.cs
+++ Assets/Scripts/UI/UIPanel.cs
@@ -10,9 +10,15 @@ public abstract class UIPanel : MonoBehaviour
 	[SerializeField]
 	private RectTransform		_view;
 	[SerializeField]
-	private UIButton			_okButton;
+	private float				_showDuration			= 1f;
 	[SerializeField]
-	private float				_duration	= 1f;
+	private RectTransform       _text;
+	[SerializeField]
+	private float               _textShowDuration		= 0.5f;
+	[SerializeField]
+	private UIButton            _okButton;
+	[SerializeField]
+	private float				_buttonShowDuration		= 0.25f;
 
 	private LevelCanvas			_levelCanvas;
 	protected LevelSceneLoader  _levelSceneLoader;
@@ -27,7 +33,9 @@ public abstract class UIPanel : MonoBehaviour
 	{
 		_body.anchoredPosition = Vector2.zero;
 		_body.sizeDelta = Vector2.zero;
-		_view.transform.localScale = Vector3.zero;
+		_okButton.transform.localScale = Vector2.zero;
+		_text.localScale = Vector2.zero;
+		_view.localScale = Vector3.zero;
 		_view.gameObject.SetActive(false);
 	}
 
@@ -45,13 +53,19 @@ public abstract class UIPanel : MonoBehaviour
 	{
 		_levelCanvas.ShowBackground();
 		_view.gameObject.SetActive(true);
-		_view.transform.DOScale(Vector3.one, _duration).SetEase(Ease.OutBack);
+		_view.transform.DOScale(Vector3.one, _showDuration).SetEase(Ease.OutBack).OnComplete(() =>
+		{
+			_text.DOScale(Vector3.one, _textShowDuration).SetEase(Ease.OutBack).OnComplete(() =>
+			{
+				_okButton.transform.DOScale(Vector3.one, _buttonShowDuration).SetEase(Ease.OutBack);
+			});
+		});
 	}
 
 	public virtual void Hide()
 	{
 		_levelCanvas.HideBackground();
-		_view.transform.DOScale(Vector3.zero, _duration).SetEase(Ease.InBack).OnComplete(() =>
+		_view.transform.DOScale(Vector3.zero, _showDuration).SetEase(Ease.InBack).OnComplete(() =>
 		{
 			_view.gameObject.SetActive(false);
 			OnHideComplete();
